5c0f8b0f88b49651f74c39fe5014218846c5665a
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-10 08:31:37 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'genre/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "genre#index"
@@ -7,4 +6,5 @@ Rails.application.routes.draw do
   resources :genres do
     resources :categories
   end
+  resources :talks
 end
