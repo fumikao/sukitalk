37371957fcdd100c1c23798dab500578cb616b21
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-09 10:56:19 +0900

    rootパス変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'genre/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "genre#index"
   resources :users, only: [:index, :show]
   resources :genres
 end
