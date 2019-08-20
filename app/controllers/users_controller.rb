fc91fe9d58f3a036de33c49bf0be608236685ee2
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-20 19:16:55 +0900

    search機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
 
   def show
@@ -8,5 +10,16 @@ class UsersController < ApplicationController
     @user.category_users.each do |category_user|
       @categories << category_user.category
     end
+  end
+
+  def search
+    @q = User.search(search_params)
+    @users = @q.result(distinct: true)
+  end
+
+  private
+
+  def search_params
+    params.require(:q).permit(:nickname_cont, :sex_eq, :address_eq)
   end
 end
